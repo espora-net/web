3b66817f121616c0b708a59716eab2af5696d1e0
Carlos <carlos.hm@live.com>
2019-02-01 19:30:31 +0100

    Create goodreads.js

---
+++ assets/js/goodreads.js
@@ -0,0 +1,24 @@
+$(function(){
+  var url = 'https://www.goodreads.com/review/list_rss/92994383?key=3yng7ZCmInuRcXLjpbDyrJRYe_he__uLfNygmatNKCB3ZwFv&shelf=espora';
+  var books = $('.books');
+
+  function loadBooks(url) {
+    $.ajax({
+      url: url,
+      type: 'GET',
+      dataType: "xml"
+    })
+    .done(function(xml) {
+			$(xml).find("channel item").each(function () {
+				var book = $(this);
+        books.append('<div class="4u"><span class="image fit">');
+        books.append('<img src="' + book.find("book_medium_image_url").text() + '" alt="' + book.find("book_description").text() + '" />' + book.find("title").text());
+        books.append('</span></div>');
+			});      
+    })
+    .fail(function(){
+      books.hide();
+    });
+  }
+  loadBooks(url);
+});
