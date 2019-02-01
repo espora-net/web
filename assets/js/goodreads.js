3348aee6a09e79e972f6d6fc34f7f6f5ac8679b3
Carlos <carlos.hm@live.com>
2019-02-01 22:00:42 +0100

    Update goodreads.js

--- assets/js/goodreads.js
+++ assets/js/goodreads.js
@@ -12,7 +12,7 @@ $(function(){
     .done(function(xml) {
 	$(xml).find("review book").each(function () {
 	var book = $(this);
-        books.append('<div class="3u"><span class="image fit"><img src="' + book.find("image_url").first().text() + '" alt="' + book.find("description").text() + '" />' + book.find("title").text() + '</span></div>');
+        books.append('<div class="2u"><span class="image fit"><img src="' + book.find("image_url").first().text() + '" alt="' + book.find("description").text() + '" />' + book.find("title").text() + '</span></div>');
 	});      
     })
     .fail(function(){
