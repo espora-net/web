1c5c49cd3fd61c73b5e326c6229859ac222f8f2a
Carlos <carlos.hm@live.com>
2019-02-02 09:11:30 +0100

    Update goodreads.js

--- assets/js/goodreads.js
+++ assets/js/goodreads.js
@@ -1,23 +1,34 @@
 $(function(){
   // for the future CORS GoodReads API var url = 'https://www.goodreads.com/review/list/92994383.xml?v=2&shelf=espora&per_page=200&page=1&key=';
-  var url = '/assets/92994383.xml';
-  var books = $('.books');
+  var novel = '/assets/novel.xml';
+  var business = '/assets/business.xml';
+  var scifi = '/assets/scifi.xml';
+  var biography = '/assets/biography.xml';
+  var childrenteens = '/assets/children-teens.xml';
+  var personaldevelopment = '/assets/personal-development.xml';
 
-  function loadBooks(url) {
+  function loadBooks(url, shelf) {
     $.ajax({
       url: url,
       type: 'GET',
       dataType: "xml"
     })
     .done(function(xml) {
+	shelf.append('<div class="12u"><span class="image fit"><img src="images/' + shelf + '.jpg" alt="" /></span></div>');
 	$(xml).find("review book").each(function () {
-	var book = $(this);
-        books.append('<div class="1u"><span class="image fit"><a href="' + book.find("link").first().text() + '"><img src="' + book.find("image_url").first().text() + '" alt="' + book.find("title").text() + '" /></a></span></div>');
+	  var book = $(this);
+          shelf.append('<div class="1u"><span class="image fit"><a href="' + book.find("link").first().text() + '"><img src="' + book.find("image_url").first().text() + '" alt="' + book.find("title").text() + '" /></a></span></div>');
 	});      
     })
     .fail(function(){
       books.hide();
     });
   }
-  loadBooks(url);
+	
+  loadBooks(url, $('.novel'));
+  loadBooks(url, $('.business'));
+  loadBooks(url, $('.scifi'));
+  loadBooks(url, $('.biography'));
+  loadBooks(url, $('.childrenteens'));
+  loadBooks(url, $('.personaldevelopment'));
 });
